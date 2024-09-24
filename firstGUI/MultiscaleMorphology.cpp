b7abdccd5a6c686fa1be0381dce41ee6e5776d13
Sangyun <sxm4113@gmail.com>
2024-09-24 10:21:08 -0400

    add 9 by 9 kernel

--- firstGUI/MultiscaleMorphology.cpp
+++ firstGUI/MultiscaleMorphology.cpp
@@ -27,14 +27,16 @@ void MultiscaleMorphology::run_algorithm()
 {
 	cv::Mat tophat3 = top_hat(input_image, 2); 
 	cv::Mat tophat5 = top_hat(input_image, 4); 
-	cv::Mat tophat7 = top_hat(input_image, 6); 
+	cv::Mat tophat7 = top_hat(input_image, 6);
+	cv::Mat tophat9 = top_hat(input_image, 8);
 
 	cv::Mat blackhat3 = black_hat(input_image, 2); 
-	cv::Mat blackhat5 = black_hat(input_image, 4); 
-	cv::Mat blackhat7 = black_hat(input_image, 6); 
+	cv::Mat blackhat5 = black_hat(input_image, 4);
+	cv::Mat blackhat7 = black_hat(input_image, 6);
+	cv::Mat blackhat9 = black_hat(input_image, 8);
 
-	cv::Mat sum_blackhat = 0.9 * blackhat3 + 0.5 * blackhat5 + 0.3 * blackhat7  ;
-	cv::Mat sum_tophat = 0.9 * tophat3 + 0.5 * tophat5 + 0.3 * tophat7 ;
+	cv::Mat sum_blackhat = 0.9 * blackhat3 + 0.5 * blackhat5 + 0.3 * blackhat7 + 0.1 * blackhat9;
+	cv::Mat sum_tophat = 0.9 * tophat3 + 0.5 * tophat5 + 0.3 * tophat7 + 0.1 * tophat9;
 
 	morpology_result = input_image - sum_blackhat + sum_tophat;
 }
